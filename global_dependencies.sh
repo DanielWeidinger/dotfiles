59a33cc6e6fe0f4739b2ced93a41510d7a9de04d
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2021-10-28 13:56:38 +0200

    oh my zsh

--- global_dependencies.sh
+++ global_dependencies.sh
@@ -5,3 +5,6 @@ sudo apt install -y zsh
 sudo chsh -s /usr/bin/zsh daniel
 echo !!!!!!!!!!!!!!!!! dont forget to restart !!!!!!!!!!!!!!!!!
 
+echo installing oh-my-zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
