a01347989efe764e0ef8314eba2f2d9296a13d30
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2021-11-23 15:41:38 +0100

    scripts update

--- .config/nvim/scripts/nvim.sh
+++ .config/nvim/scripts/nvim.sh
@@ -4,8 +4,7 @@ echo Installing dependencies
 echo --Downloading Repo...
 git clone https://github.com/neovim/neovim.git
 cd neovim
-git checkout v0.5.0
-# git checkout v0.5.1
+git checkout stable
 
 echo --Installing...
 sudo make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=/usr/ install
