b5a58f12b0efa47fc5102c0e926c0b7b356c1e84
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2022-04-30 12:52:33 +0200

    nnn: fix

--- stow/.config/nnn/nnnrc.sh
+++ stow/.config/nnn/nnnrc.sh
@@ -1,12 +1,11 @@
 # cd on quit
 export NNN_PLUG="p:preview-tui;"
 
-
 if [ -f /usr/share/nnn/quitcd/quitcd.bash_zsh ]; then
     source /usr/share/nnn/quitcd/quitcd.bash_zsh
 fi
 
 alias nnn "nnn -e"
 
-set --export NNN_FIFO "/tmp/nnn.fifo"
+export NNN_FIFO="/tmp/nnn.fifo"
 
