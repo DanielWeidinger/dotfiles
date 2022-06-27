903146cb22f5f0391e2815d924c0d722537ddab2
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2022-06-27 15:18:09 +0200

    nvim: lsp bindings

--- stow/.config/nvim/lua/weidinger/lsp/init.lua
+++ stow/.config/nvim/lua/weidinger/lsp/init.lua
@@ -5,23 +5,22 @@ Capabilities.textDocument.completion.completionItem.snippetSupport = true
 -- LSP config (the mappings used in the default file don't quite work right)
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
-keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+-- keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
 keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+-- keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+-- keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 
-keymap("n", "<C-n>", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-keymap("n", "<C-p>", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+-- keymap("n", "<C-n>", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+-- keymap("n", "<C-p>", "<cmd>lua vim.diagnostic.goto_next()<CR>")
 
 local wk = require("which-key")
 -- Using trouble for navigation rather than QL list
 wk.register({
 	g = {
-		-- d = { '<cmd>lua vim.lsp.buf.definition()<CR>', 'Go to definition' },
 		D = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "Go to declaration" },
-		-- R = { '<cmd>lua vim.lsp.buf.references()<CR>', 'Open references' },
-		-- i = { '<cmd>lua vim.lsp.buf.implementation()<CR>', 'Go to implementation' },
 	},
+	["Ö"] = { "<cmd>lua vim.diagnostic.goto_next()<CR>", "Go to next Diagnostic" },
+	["Ä"] = { "<cmd>lua vim.diagnostic.goto_prev()<CR>", "Go to next Diagnostic" },
 })
 
 require("weidinger.lsp.lsp")
