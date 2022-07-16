0aef7708219fde488fddbe03a5fd3e6123a86ad7
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2022-07-16 21:29:06 +0200

    nvim: dap-ui WIP

--- stow/.config/nvim/lua/weidinger/dap.lua
+++ stow/.config/nvim/lua/weidinger/dap.lua
@@ -5,7 +5,37 @@ dap.defaults.fallback.external_terminal = {
 }
 -- dap.defaults.fallback.force_external_terminal = true
 
-dapui.setup()
+dapui.setup({
+	layouts = {
+		{
+			elements = {
+				{ id = "scopes", size = 0.25 },
+				{ id = "watches", size = 0.25 },
+				{ id = "stacks", size = 0.25 },
+				{ id = "breakpoints", size = 0.25 },
+			},
+			size = 0.25,
+			position = "left",
+		},
+		{
+			elements = { "repl" },
+			size = 0.25,
+			position = "bottom",
+		},
+	},
+	floating = {
+		max_height = nil, -- These can be integers or a float between 0 and 1.
+		max_width = nil, -- Floats will be treated as percentage of your screen.
+		border = "single", -- Border style. Can be "single", "double" or "rounded"
+		mappings = {
+			close = { "q", "<Esc>" },
+		},
+	},
+	windows = { indent = 1 },
+	render = {
+		max_type_length = nil, -- Can be integer or nil.
+	},
+})
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
 	dapui.open()
