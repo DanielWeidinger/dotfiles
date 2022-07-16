bace8fc2af1b738444d96d5fe55add25a016c455
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2022-07-16 21:29:06 +0200

    nvim: dap-ui ergonomics

--- stow/.config/nvim/lua/weidinger/dap.lua
+++ stow/.config/nvim/lua/weidinger/dap.lua
@@ -5,7 +5,37 @@ dap.defaults.fallback.external_terminal = {
 }
 -- dap.defaults.fallback.force_external_terminal = true
 
-dapui.setup()
+dapui.setup({
+	layouts = {
+		{
+			elements = {
+				{ id = "watches", size = 0.25 },
+				{ id = "scopes", size = 0.5 },
+				{ id = "stacks", size = 0.25 },
+				-- { id = "breakpoints", size = 0.25 },
+			},
+			size = 0.2,
+			position = "left",
+		},
+		{
+			elements = { "repl" },
+			size = 0.25,
+			position = "bottom",
+		},
+	},
+	floating = {
+		max_height = nil, -- These can be integers or a float between 0 and 1.
+		max_width = nil, -- Floats will be treated as percentage of your screen.
+		border = "single", -- Border style. Can be "single", "double" or "rounded"
+		mappings = {
+			close = { "q", "<Esc>" },
+		},
+	},
+	windows = { indent = 1 },
+	render = {
+		max_type_length = nil, -- Can be integer or nil.
+	},
+})
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
 	dapui.open()
@@ -67,9 +97,10 @@ wk.register({
 		-- ["<leader>"] = { ":lua require'dap'.continue()<cr>", "Continue" },
 		r = { ":lua require'dap'.run_to_cursor()<cr>", "ToCursor" },
 		-- b = { ":lua require'dap'.toggle_breakpoint()<cr>", "◯ :Toggle" },
-		i = { ":lua require('dap.ui.widgets').hover()<cr>", "Eval under cursor" },
+		i = { ":lua require'dap.ui.widgets'.hover()<cr>", "Eval under cursor" },
 		o = { ":lua require'dapui'.toggle()<cr>", "Toggle full UI" },
 		O = { ":lua require'dapui'.open()<cr>", "Open full UI" },
+		w = { ":Telescope dap list_breakpoints<cr>", "Open Watches" },
 	},
 	["<leader>"] = { ":lua require'dap'.continue()<cr>", "Continue" },
 	b = { ":lua require'dap'.toggle_breakpoint()<cr>", "◯ :Toggle" },
