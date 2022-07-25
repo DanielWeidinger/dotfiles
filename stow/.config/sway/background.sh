20b502d728d533f194b3076367f51d95036e85be
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2022-07-25 13:58:27 +0200

    sway: wallpapers

--- stow/.config/sway/background.sh
+++ stow/.config/sway/background.sh
@@ -4,12 +4,12 @@ if [ -z ${WALLPAPER_DIR+x} ]; then
     source $HOME/.zshenv
 fi
 
-swaybg -i $(find $WALLPAPER_DIR -type f | shuf -n1) -m fill &
+swaybg -i "$WALLPAPER_DIR/$(ls $WALLPAPER_DIR | shuf -n1)" -m fill &
 OLD_PID=$!
 
 while true; do
     sleep 3600 # change every hour
-    swaybg -i $(find $WALLPAPER_DIR -type f | shuf -n1) -m fill &
+    swaybg -i "$WALLPAPER_DIR/$(ls $WALLPAPER_DIR | shuf -n1)" -m fill &
     NEXT_PID=$!
     sleep 5
     kill $OLD_PID
