26ebb799b37af37d4f7c6d1a49883f77581ef570
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2022-09-17 14:39:26 +0200

    nvim: tabnine cmp

--- stow/.config/nvim/lua/weidinger/lsp/cmp.lua
+++ stow/.config/nvim/lua/weidinger/lsp/cmp.lua
@@ -3,17 +3,12 @@ vim.o.completeopt = "menu,menuone,noselect"
 -- Setup nvim-cmp.
 local cmp = require("cmp")
 local lspkind = require("lspkind") -- Better iconography for completion
-
--- NOTE: Copilot setup
--- vim.schedule(function() require("copilot").setup({plugin_manager_path="~/.vim/plugged"}) end)
+local compare = require("cmp.config.compare")
 
 cmp.setup({
 	snippet = {
 		expand = function(args)
 			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-			-- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
 		end,
 	},
 	mapping = {
@@ -22,28 +17,48 @@ cmp.setup({
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
 		["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-		-- ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-		-- ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
 		["<C-e>"] = cmp.mapping.close(),
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
 	},
 	sources = cmp.config.sources({
-		-- { name = "copilot", group_index = 2},
 		{ name = "nvim_lsp" },
 		{ name = "vsnip" }, -- For vsnip users.
-		-- { name = 'luasnip' }, -- For luasnip users.
-		-- { name = 'ultisnips' }, -- For ultisnips users.
-		-- { name = 'snippy' }, -- For snippy users.
-	}, {
 		{ name = "buffer" },
 		{ name = "path" },
 		{ name = "spell" },
 		{ name = "calc" },
 		{ name = "treesitter" },
 		{ name = "dap" },
+		{ name = "cmp_tabnine" },
 	}),
 	formatting = {
-		format = lspkind.cmp_format({ with_text = true }),
+		format = lspkind.cmp_format({
+			mode = "symbol_text",
+			menu = {
+				buffer = "[Bff]",
+				nvim_lsp = "[LSP]",
+				vsnip = "[VSnp]",
+				cmp_tabnine = "[Tb9]",
+				treesitter = "[]",
+				spell = "[暈]",
+				dap = "[DAP]",
+				calc = "[]",
+			},
+		}),
+	},
+	sorting = {
+		priority_weight = 2,
+		comparators = {
+			require("cmp_tabnine.compare"),
+			compare.offset,
+			compare.exact,
+			compare.score,
+			compare.recently_used,
+			compare.kind,
+			compare.sort_text,
+			compare.length,
+			compare.order,
+		},
 	},
 	enabled = function()
 		return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
@@ -52,6 +67,3 @@ cmp.setup({
 
 -- enable spellling suggestions
 vim.opt.spelllang = { "en_us" }
-
--- Setup lspconfig in lsp.lua
--- Setup autopairing in autoclosing.lua
