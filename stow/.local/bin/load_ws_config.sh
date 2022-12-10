8297047eebdc1d2d13c5514ca221325ea7693773
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2022-12-10 14:44:13 +0100

    scripts: workspaces

--- stow/.local/bin/load_ws_config.sh
+++ stow/.local/bin/load_ws_config.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
 N_DISPLAYS=$(swaymsg -t get_outputs | jq length)
+echo $N_DISPLAYS
 
 for ws in $(jq '.[].ws' $1); do
-    CMD=$(jq ".[] | select(.ws == $ws)" $1 | jq .cmd)
-    DELAY=$(jq ".[] | select(.ws == $ws)" $1 | jq .delay)
-    echo $ws
-    if [[ $ws = -1 ]]; then
-        swaymsg "workspace 1; exec $CMD"
-        sleep $DELAY
-        swaymsg "move scratchpad"
-    else
-        echo "starting $CMD at $ws"
-        swaymsg "workspace $ws; exec $CMD"
-        sleep $DELAY
-    fi
+	CMD=$(jq ".[] | select(.ws == $ws)" $1 | jq .cmd)
+	DELAY=$(jq ".[] | select(.ws == $ws)" $1 | jq .delay)
+	echo $ws
+
+	if [[ $ws = -1 ]]; then
+		swaymsg "workspace 1; exec $CMD"
+		sleep $DELAY
+		swaymsg "move scratchpad"
+	else
+		echo "starting $CMD at $ws"
+		swaymsg "workspace $ws; exec $CMD"
+		sleep $DELAY
+	fi
 done
 
 #Reset to first workspaces
 for ((display = 1; display <= $N_DISPLAYS; display++)); do
-    swaymsg "workspace $display"
+	swaymsg "workspace $display"
 done
 
 exit 0
