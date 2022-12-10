f66f7053a2b9034b9fbd4be9a297546830292337
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2022-12-10 13:06:45 +0100

    scripts: nvim dap installation fix

--- stow/.config/nvim/scripts/dap_setup.sh
+++ stow/.config/nvim/scripts/dap_setup.sh
@@ -1,8 +1,8 @@
 dap_dir="$HOME/.local/share/nvim/debugger"
-cd $dap_dir
 if [ ! -d "$dap_dir" ]; then
     mkdir $dap_dir
 fi
+cd $dap_dir
 
 echo --NodeJs Debugger
 git clone git@github.com:microsoft/vscode-js-debug.git
