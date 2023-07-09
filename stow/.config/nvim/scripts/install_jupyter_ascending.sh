1afb750814184f19e59cf838da2e208148fdc0fe
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2023-07-09 18:05:13 +0200

    nvim: scripts

--- stow/.config/nvim/scripts/install_jupyter_ascending.sh
+++ stow/.config/nvim/scripts/install_jupyter_ascending.sh
@@ -4,7 +4,7 @@ if $# -eq 0; then
 	exit -1
 fi
 
-python -m ipykernel install --user --name $1 --display-name "Python ($1)"
+# python -m ipykernel install --user --name $1 --display-name "Python ($1)"
 
 pip install jupyter_ascending
 # INFO: If you (don't) use conda add/remove the --sys-prefix/--user (https://github.com/jupyter/notebook/issues/1731)
--- stow/.config/nvim/scripts/lsp_setup.sh
+++ stow/.config/nvim/scripts/lsp_setup.sh
@@ -14,7 +14,7 @@ echo --Bash
 sudo npm install -g bash-language-server
 
 echo --Bash Formatting
-sudo pacman -S shfmt
+sudo pacman -S shfmt --noconfirm
 
 echo --JSON, CSS and HTML
 sudo npm install -g vscode-langservers-extracted
@@ -28,14 +28,14 @@ sudo npm install -g emmet-ls
 echo --eslint + prettier
 sudo npm install -g eslint_d @fsouza/prettierd
 
+echo --Lua
 langserver_dir="$HOME/.local/share/nvim/lang-servers"
 if [ ! -d "$langserver_dir" ]; then
 	mkdir $langserver_dir
 fi
-echo --Lua
 sudo pacman -S stylua --noconfirm
 cd $langserver_dir
-git clone https://github.com/sumneko/lua-language-server
+git clone https://github.com/LuaLS/lua-language-server
 cd lua-language-server
 git submodule update --init --recursive
 cd 3rd/luamake
@@ -43,15 +43,15 @@ cd 3rd/luamake
 cd ../..
 ./3rd/luamake/luamake rebuild
 
-echo --OmniSharp
-cd $langserver_dir
-if [ ! -d "omnisharp" ]; then
-	mkdir omnisharp
-fi
-cd omnisharp
-curl -s https://api.github.com/repos/OmniSharp/omnisharp-roslyn/releases/latest |
-	grep "browser_download_url.*omnisharp-linux-x86.tar.gz" |
-	cut -d : -f 2,3 |
-	tr -d \" |
-	wget -qi -
-tar -xzf omnisharp-linux-x86.tar.gz
+# echo --OmniSharp
+# cd $langserver_dir
+# if [ ! -d "omnisharp" ]; then
+# 	mkdir omnisharp
+# fi
+# cd omnisharp
+# curl -s https://api.github.com/repos/OmniSharp/omnisharp-roslyn/releases/latest |
+# 	grep "browser_download_url.*omnisharp-linux-x86.tar.gz" |
+# 	cut -d : -f 2,3 |
+# 	tr -d \" |
+# 	wget -qi -
+# tar -xzf omnisharp-linux-x86.tar.gz
