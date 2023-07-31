efe3625affa70850cd747126e7b2d66f168901ed
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2023-07-31 17:26:04 +0200

    sway: fix wallpaper script

--- stow/.config/sway/background.sh
+++ stow/.config/sway/background.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
 if [ -z ${WALLPAPER_DIR+x} ]; then
-    source $HOME/.zshenv
+	source $HOME/.zshenv
 fi
+function change_wp() {
+	swaybg -i "$(ls $WALLPAPER_DIR/*.jpg | shuf -n1)" -m fill &
+}
 
-swaybg -i "$WALLPAPER_DIR/$(ls $WALLPAPER_DIR | shuf -n1)" -m fill &
 OLD_PID=$!
+change_wp
 
 while true; do
-    sleep 3600 # change every hour
-    swaybg -i "$WALLPAPER_DIR/$(ls $WALLPAPER_DIR | shuf -n1)" -m fill &
-    NEXT_PID=$!
-    sleep 5
-    kill $OLD_PID
-    OLD_PID=$NEXT_PID
+	sleep 3600 # change every hour
+    change_wp
+	NEXT_PID=$!
+	sleep 5
+	kill $OLD_PID
+	OLD_PID=$NEXT_PID
 done
