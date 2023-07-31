3796913570c097dcf54e750c70a9db7fc26b4601
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2023-07-31 10:23:48 +0200

    scripts: formatting

--- scripts/setup_os.sh
+++ scripts/setup_os.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if ! [ "$EUID" -ne 0 ]
-  then echo "Do not run as root"
-  exit
+if ! [ "$EUID" -ne 0 ]; then
+	echo "Do not run as root"
+	exit
 fi
 
 sudo pacman -S reflector --noconfirm
@@ -10,20 +10,19 @@ reflector -c Austria -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 sudo pacman -Syuu --noconfirm
 sudo pacman -Syyu --noconfirm
 
-
 GRAPHICAL_INTERFACE="wayland kanshi sway waybar xorg-xlsclients swaybg swaylock"
 sudo pacman -S $GRAPHICAL_INTERFACE --noconfirm
 
 DRIVERS="pipewire pipewire-alsa pipewire-jack pipewire-pulse bluez-utils alsa-firmware"
 sudo pacman -S $DRIVERS --noconfirm
 
-BASE_DEPS="networkmanager zsh git curl wget go npm ninja stow jq ripgrep"
+BASE_DEPS="networkmanager zsh git curl wget go npm ninja stow jq ripgrep wl-clipboard"
 sudo pacman -S $BASE_DEPS --noconfirm
 
 DEV_DEPS="docker dotnet-sdk mono mono-msbuild"
 sudo pacman -S $BASE_DEPS --noconfirm
 
-GUI_DEPS="wofi feh alacritty firefox chromium"
+GUI_DEPS="wofi feh alacritty firefox chromium nautlius"
 sudo pacman -S $GUI_DEPS --noconfirm
 
 # Start services
@@ -31,13 +30,12 @@ sudo systemctl enable --now NetworkManager.service
 sudo systemctl enable --now systemd-resolved.service # DNS caching
 
 # Install yay
-if ! command -v yay &> /dev/null
-then
-    git clone https://aur.archlinux.org/yay.git
-    cd yay
-    makepkg -si
-    cd ..
-    rm -rf yay
+if ! command -v yay &>/dev/null; then
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg -si
+	cd ..
+	rm -rf yay
 fi
 
 MISC_UTIL_DEPS="dunst light pamixer pavucontrol playerctl grim slurp trash-cli brightnessctl"
@@ -67,14 +65,14 @@ sudo systemctl enable --now ssh-agend.service --user
 
 # OhMyZsh
 if ! [ -d ~/.oh-my-zsh ]; then
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# miniconda/python 
+# miniconda/python
 INSTALL_SCRIPT_PATH="/tmp/install_minionda.sh"
-curl https://repo.anaconda.com/miniconda/Miniconda3-py37_4.12.0-Linux-x86_64.sh > $INSTALL_SCRIPT_PATH
+curl https://repo.anaconda.com/miniconda/Miniconda3-py37_4.12.0-Linux-x86_64.sh >$INSTALL_SCRIPT_PATH
 chmod +x $INSTALL_SCRIPT_PATH
 zsh $INSTALL_SCRIPT_PATH
 conda config --set auto_activate_base false
@@ -85,7 +83,7 @@ zsh ~/.dotfiles/stow/.config/nvim/scripts/dependencies.sh
 ./stow/.config/nvim/scripts/lsp_setup.sh
 
 # Install grimshot for screenshoting
-curl https://raw.githubusercontent.com/swaywm/sway/master/contrib/grimshot > /tmp/grimshot
+curl https://raw.githubusercontent.com/swaywm/sway/master/contrib/grimshot >/tmp/grimshot
 sudo mv /tmp/grimshot /usr/bin
 sudo chmod +x /usr/bin/grimshot
 
