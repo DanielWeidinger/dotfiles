2fdfb09edbac2af6bebf0df0a4f910ee7288d4da
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2023-12-03 12:24:23 +0100

    nvim: harpoon update

--- stow/.config/nvim/lua/weidinger/UI/harpoon.lua
+++ stow/.config/nvim/lua/weidinger/UI/harpoon.lua
@@ -1,19 +1,27 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
 local keymap = vim.keymap
 
-keymap.set("n", "<A-j>", mark.add_file)
-keymap.set("n", "<A-e>", ui.toggle_quick_menu)
+keymap.set("n", "<A-j>", function()
+    harpoon:list():append()
+end)
+keymap.set("n", "<A-e>", function()
+    harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
 
 keymap.set("n", "<A-f>", function()
-    ui.nav_file(1)
+    harpoon:list():select(1)
 end)
 keymap.set("n", "<A-d>", function()
-    ui.nav_file(2)
+    harpoon:list():select(2)
 end)
 keymap.set("n", "<A-s>", function()
-    ui.nav_file(3)
+    harpoon:list():select(3)
 end)
 keymap.set("n", "<A-a>", function()
-    ui.nav_file(4)
+    harpoon:list():select(4)
 end)
