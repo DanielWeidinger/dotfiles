806a789c6fd1cc1d09aa2b7b30507b02c4114523
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2024-06-16 13:35:24 +0200

    nvim: trouble fix and co

--- stow/.config/nvim/lua/weidinger/utils/autoclosing.lua
+++ stow/.config/nvim/lua/weidinger/utils/autoclosing.lua
@@ -5,9 +5,4 @@ local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp = require("cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 
-require("nvim-treesitter.configs").setup({
-    autotag = {
-        enable = true,
-    },
-})
--- require('nvim-ts-autotag').setup{}
+require("nvim-ts-autotag").setup({})
